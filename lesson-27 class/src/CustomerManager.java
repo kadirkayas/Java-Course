a245cb852fbc8126791f0d1832a6f0c77a055baa
cmwkadir <kadirkayas2208@gmail.com>
2024-08-02 12:35:37 +0900

    0

---
+++ lesson-27 class/src/CustomerManager.java
@@ -0,0 +1,12 @@
+public class CustomerManager
+{
+    public void Add(){
+        System.out.println("Customer added");
+    }
+    public void Remove(){
+        System.out.println("Customer removed");
+    }
+    public void Update(){
+        System.out.println("Customer updated");
+    }
+}
--- lesson-27 class/src/Main.java
+++ lesson-27 class/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        CustomerManager cm;
+        CustomerManager cm2 =new CustomerManager();
+        cm=cm2;
+        cm2.Add();
     }
 }
